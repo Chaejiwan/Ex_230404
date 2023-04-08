d117c7bc82b179428db829b57a2692666c1a774f
jiwan <jiwan5605@naver.com>
2023-04-08 11:10:55 +0900

    commit3

--- src/main/java/com/multicampus/ex01/controller/BoardController.java
+++ src/main/java/com/multicampus/ex01/controller/BoardController.java
@@ -11,8 +11,13 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/board")
@@ -28,4 +33,25 @@ public class BoardController {
         log.info(responseDTO);
         model.addAttribute("responseDTO", responseDTO);
     }
+
+    @GetMapping("/register")
+    public void registerGET() {
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO
+                                 , BindingResult bindingResult
+                                 , RedirectAttributes redirectAttributes) {
+        if(bindingResult.hasErrors()) {
+            log.info("has errors...");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/board/register";
+        }
+        log.info(boardDTO);
+        Long bno = boardService.register(boardDTO);
+        redirectAttributes.addFlashAttribute("result",bno);
+        return "redirect:/board/list";
+    }
+
 }
--- src/main/java/com/multicampus/ex01/dto/BoardDTO.java
+++ src/main/java/com/multicampus/ex01/dto/BoardDTO.java
@@ -4,18 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import net.bytebuddy.asm.Advice;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
-@Data
+@Data //@Valid 어노테이션 추가 학습 하기
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class BoardDTO {
     private Long bno;
+
+    @NotEmpty
+    @Size(min=3, max=100)
     private String title;
+    @NotEmpty
     private String content;
+    @NotEmpty
     private String writer;
     private LocalDateTime regDate;
     private LocalDateTime modDate;
